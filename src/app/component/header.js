ef39a54ae0846146ff07a4ec1cde6368e05704ab
Your Name <you@example.com>
2026-08-10 11:37:29 +0530

    Update header hearts and background

--- src/app/component/header.js
+++ src/app/component/header.js
@@ -4,6 +4,11 @@ export default function Header() {
             <div className="title__container">
                 <h4>Welcome To My Page Gunavathi Kuppan</h4>
             </div>
+            <div className="hearts">
+                <span className="heart">♥</span>
+                <span className="heart">♥</span>
+                <span className="heart">♥</span>
+            </div>
         </header>
     );
 }
