374a9bb47ab7cceddcf55e404f20aa837619a92c
Your Name <you@example.com>
2026-08-10 10:41:05 +0530

    Auto commit: save before push

--- src/app/component/header.js
+++ src/app/component/header.js
@@ -2,9 +2,8 @@ export default function Header() {
     return (
         <header className="title">
             <div className="title__container">
-                <h1>Welcome To My Page Gunavathi Kuppan</h1> 
-                </div>
-            
+                <h4>Welcome To My Page Gunavathi Kuppan</h4>
+            </div>
         </header>
     );
 }
