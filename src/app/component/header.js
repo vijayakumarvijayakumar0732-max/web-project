8fc02955bb995b95eac7e349877770427d6c430f
Your Name <you@example.com>
2026-08-10 10:18:45 +0530

    Initial commit

---
+++ src/app/component/header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>My App</h1>
+            
+        </header>
+    );
+}
---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import header from './component/header';
+
+export default function Page() {
+    return (
+        <div>
+       
+           
+        </div>
+    );
+}
